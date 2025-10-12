7bd57d2867f9135cdf32453fdbc92aa8a14c9f96
scriptedd_ <oguzturan0706@gmail.com>
2025-10-12 22:23:24 +0300

    Loop for more calculations

--- src/main.go
+++ src/main.go
@@ -13,80 +13,82 @@ import (
 )
 
 func main() {
+	
 	var x int
 	var y int
+	var run bool = true
 	var mode string
 
 	fmt.Print("1. Adding\n")
 	fmt.Print("2. Subtraction\n")
 	fmt.Print("3. Multiplying\n")
 	fmt.Print("0. Exit")
-	fmt.Print("\n> ")
+	
+	for run {
+		fmt.Print("\n> ")
+		fmt.Scanln(&mode)
 
-	fmt.Scanln(&mode)
+		switch mode {
+		case "1":
+			fmt.Print("\nEnter a number: ")
 
-	switch mode {
-	case "1":
-		fmt.Print("\nEnter a number: ")
+			_, err1 := fmt.Scanln(&x)
+			if err1 != nil {
+				fmt.Print("An error has occured: ", err1)
+				return
+			}
 
-		_, err1 := fmt.Scanln(&x)
-		if err1 != nil {
-			fmt.Print("An error has occured: ", err1)
-			return
+			fmt.Print("Enter another number: ")
+
+			_, err2 := fmt.Scanln(&y)
+			if err2 != nil {
+				fmt.Print("An error has occured: ", err2)
+				return
+			}
+
+			fmt.Print(x, " + ", y ," = ", x + y)
+		case "2":
+			fmt.Print("\nEnter a number: ")
+
+			_, err1 := fmt.Scanln(&x)
+			if err1 != nil {
+				fmt.Print("An error has occured: ", err1)
+				return
+			}
+
+			fmt.Print("Enter another number: ")
+
+			_, err2 := fmt.Scanln(&y)
+			if err2 != nil {
+				fmt.Print("An error has occured: ", err2)
+				return
+			}
+
+			fmt.Print(x, " - ", y ," = ", x - y)
+		case "3":
+			fmt.Print("\nEnter a number: ")
+
+			_, err1 := fmt.Scanln(&x)
+			if err1 != nil {
+				fmt.Print("An error has occured: ", err1)
+				return
+			}
+
+			fmt.Print("Enter another number: ")
+
+			_, err2 := fmt.Scanln(&y)
+			if err2 != nil {
+				fmt.Print("An error has occured: ", err2)
+				return
+			}
+
+			fmt.Print(x, " * ", y ," = ", x * y)
+		case "0":
+			fmt.Print("Exiting...")
+			run = false
+		default:
+			fmt.Print("An error has occured: unknown mode")
+			fmt.Print("\n")
 		}
-
-		fmt.Print("Enter another number: ")
-
-		_, err2 := fmt.Scanln(&y)
-		if err2 != nil {
-			fmt.Print("An error has occured: ", err2)
-			return
-		}
-
-		fmt.Print(x, " + ", y ," = ", x + y)
-	case "2":
-		fmt.Print("\nEnter a number: ")
-
-		_, err1 := fmt.Scanln(&x)
-		if err1 != nil {
-			fmt.Print("An error has occured: ", err1)
-			return
-		}
-
-		fmt.Print("Enter another number: ")
-
-		_, err2 := fmt.Scanln(&y)
-		if err2 != nil {
-			fmt.Print("An error has occured: ", err2)
-			return
-		}
-
-		fmt.Print(x, " - ", y ," = ", x - y)
-	case "3":
-		fmt.Print("\nEnter a number: ")
-
-		_, err1 := fmt.Scanln(&x)
-		if err1 != nil {
-			fmt.Print("An error has occured: ", err1)
-			return
-		}
-
-		fmt.Print("Enter another number: ")
-
-		_, err2 := fmt.Scanln(&y)
-		if err2 != nil {
-			fmt.Print("An error has occured: ", err2)
-			return
-		}
-
-		fmt.Print(x, " * ", y ," = ", x * y)
-	case "0":
-		fmt.Print("Exiting...")
-	default:
-		fmt.Print("An error has occured: unknown mode")
-		fmt.Print("\n")
 	}
-
-	fmt.Print("\nPress ENTER to exit...\n")
-	fmt.Scanln()
 }
